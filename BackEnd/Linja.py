c8992674e9760f2a5cdbf5fdef49f2bffb388cb9
BitzKort <bitzkort@gmail.com>
2023-12-19 21:32:32 -0500

    avance back 2.0

--- BackEnd/Linja.py
+++ BackEnd/Linja.py
@@ -13,6 +13,12 @@ class Linja:
         self.tablero = tablero
         self.cantidadMovimiento = cantidadMovimiento
     
+    def buscar_espacio_libre(self, columna):
+        for fila in range(0, 6): 
+            if self.tablero[fila][columna] == 0: #preguntamos si puede avanzar, si el espacio esta disponible, lo que hace el for es verificar desde el inicio de la columna si hay un espacio disponible
+                return [fila, columna]
+        return None
+
     def avanzarPosiblesMovimientosIA(self):
 
         actualidad = self.tablero
@@ -48,54 +54,55 @@ class Linja:
                                         break
                                     
                                     else: 
-                                        for aux in range(0, 6): #hacemos un for, ya que si el espacio directamente al frente no esta disponible debemos de encontrar un espacio en esa misma columna
                                         
-                                            if not espacioLibre: # si no se ha encontrado un espacio libre en la misma columna...
-                                                if actualidad[aux][columna + 1] == 0: #preguntamos si puede avanzar, si el espacio esta disponible, lo que hace el for es verificar desde el inicio de la columna si hay un espacio disponible
+                                        respuesta = self.buscar_espacio_libre(columna + 1)
+                                        
+                                        if respuesta: # sise ha encontrado un espacio libre en la misma columna... si no se encuentra pss no puede saltar, pq lo minimo para saltar seria 2 si la siguiente columna de la anterior esta disponible 
+                                                nuevoTablero = copy.deepcopy(actualidad)
+                                                nuevoTablero[respuesta[0]][respuesta[1]] = ficha #avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
+                                                nuevoTablero[posicionFichaFila][posicionFichaCol] = 0 #como avanzamos tenemos que eliminar la ficha de donde estaba
+                                                espacioLibre = True
+                                                break
+                                        
+                                        
+
+                                
+                                elif self.cantidadMovimiento > 0 and self.cantidadMovimiento != 1: #si la cantidad de movimientos es diferente a 1 y mayor a cero
+                                    
+                                    bandera = True
+
+                                    while bandera:
+                                        try:
+                                        
+                                            if actualidad[fila][columna + self.cantidadMovimiento] == 0: #preguntamos si puede avanzar, si el espacio esta disponible
+                                                nuevoTablero = copy.deepcopy(actualidad)
+                                                nuevoTablero[fila][columna + self.cantidadMovimiento] = ficha #avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
+                                                nuevoTablero[posicionFichaFila][posicionFichaCol] = 0 #como avanzamos tenemos que eliminar la ficha de donde estaba
+                                                bandera = False
+                                                espacioLibre = True
+                                                break
+                                            
+                                            else:
+
+                                                respuesta = self.buscar_espacio_libre(columna + 1)
+                                        
+                                                if respuesta: # sise ha encontrado un espacio libre en la misma columna... si no se encuentra pss no puede saltar, pq lo minimo para saltar seria 2 si la siguiente columna de la anterior esta disponible 
                                                     nuevoTablero = copy.deepcopy(actualidad)
-                                                    nuevoTablero[aux][columna + 1] = ficha #avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
+                                                    nuevoTablero[respuesta[0]][respuesta[1]] = ficha #avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
                                                     nuevoTablero[posicionFichaFila][posicionFichaCol] = 0 #como avanzamos tenemos que eliminar la ficha de donde estaba
                                                     espacioLibre = True
                                                     break
-                                
-                                elif self.cantidadMovimiento > 0 and self.cantidadMovimiento != 1: #si la cantidad de movimientos es diferente a 1 y mayor a cero
+                                                
+                                        except:
 
-                                    
-                                    try:
-                                    
-                                        if actualidad[fila][columna + self.cantidadMovimiento] == 0: #preguntamos si puede avanzar, si el espacio esta disponible
-                                            nuevoTablero = copy.deepcopy(actualidad)
-                                            nuevoTablero[fila][columna + self.cantidadMovimiento] = ficha #avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
-                                            nuevoTablero[posicionFichaFila][posicionFichaCol] = 0 #como avanzamos tenemos que eliminar la ficha de donde estaba
-                                            espacioLibre = True
-                                            break
-                                        
-                                        else:
-
-                                            if not espacioLibre:
-                                                for aux in range(0, 6): #hacemos un for, ya que si el espacio directamente al frente no esta disponible debemos de encontrar un espacio en esa misma columna
-
-                                                    if actualidad[aux][columna + self.cantidadMovimiento] == 0: #preguntamos si puede avanzar, si el espacio esta disponible, lo que hace el for es verificar desde el inicio de la columna si hay un espacio disponible
-                                                        nuevoTablero = copy.deepcopy(actualidad)
-                                                        nuevoTablero[aux][columna + self.cantidadMovimiento] = ficha #avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
-                                                        nuevoTablero[posicionFichaFila][posicionFichaCol] = 0 #como avanzamos tenemos que eliminar la ficha de donde estaba
-                                                        espacioLibre = True
-                                                        break
-                                    
-                                    except:
-
-                                        pass
+                                            self.cantidadMovimiento -=1
 
                                 
-                                if espacioLibre == True: #Si hay espacio libre en la siguiente columna y ya se realizo el cambio
+            if espacioLibre == True: #Si hay espacio libre en la siguiente columna y ya se realizo el cambio
 
-                                    pass # añadir logica para verificar que sea un movimiento valido
+                arbolListasDeListas.append(nuevoTablero)
 
-                                else: #si entra a este else es que no hay espacio libre  en la columna que tiene que avanzar segun la cantidad de movimientos que tenga
-                                    
-                                    pass # añadir logica para "saltar" esa columna y verificar en las siguientes para poder avanzar en el juego
 
-                                            #tener encuenta que ya saltar se considera un movimiento
 
 
                         
