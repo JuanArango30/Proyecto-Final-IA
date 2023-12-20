ee7ecda2cc236822de1827f9fffcafb811199123
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2023-12-20 18:49:51 -0500

    Cálculo de estados posibles en los dos turnos funcionando

--- BackEnd/Linja.py
+++ BackEnd/Linja.py
@@ -3,22 +3,41 @@ import copy
 
 
 class Linja:
-    def __init__(self, tablero, jugador, cantidadMovimiento):
+    def __init__(
+        self,
+        tablero,
+        jugador=2,
+        turnos_restantes=2,
+        cantidadMovimiento=1,
+        fichasMetaRoja=0,
+        fichasMetaNegra=0,
+    ):
         self.tablero = tablero
         self.jugador = jugador
+        self.turnos_restantes = turnos_restantes
         self.cantidadMovimiento = cantidadMovimiento
-        self.fichasMetaNegra = 0
-        self.fichasMetaRoja = 0
+        self.fichasMetaNegra = fichasMetaNegra
+        self.fichasMetaRoja = fichasMetaRoja
 
-    def impresion(self, listaDeListas: list):
-        for fila in range(len(listaDeListas)):
-            for columna in range(len(listaDeListas[0])):
-                print(listaDeListas[fila][columna])
-            print(
-                f"Fichas meta negra: {self.fichasMetaNegra}".center(50, "-")
-                + f"Fichas meta roja: {self.fichasMetaRoja}".center(50, "-"),
-                end="\n",
-            )
+    def get_tablero(self):
+        return copy.deepcopy(self.tablero)
+
+    def fin_del_juego(self) -> bool:
+        # Búsqueda de la ficha roja de más a la izquierda (porque terminan a la derecha)
+        menor_columna_rojas = 7
+        for columna in range(7, -1, -1):
+            for fila in range(5, -1, -1):
+                if self.tablero[fila][columna] == 1:
+                    menor_columna_rojas = columna
+                    break
+        # Búsqueda de la ficha negra de más a la derecha (porque terminan a la izquierda)
+        mayor_columna_negras = 0
+        for columna in range(8):
+            for fila in range(6):
+                if self.tablero[fila][columna] == 2:
+                    mayor_columna_negras = columna
+                    break
+        return menor_columna_rojas > mayor_columna_negras
 
     def buscar_espacio_libre(self, nueva_columna):
         for fila in range(0, 6):
@@ -28,139 +47,221 @@ class Linja:
                 return [fila, nueva_columna]
         return None
 
-    def avanzarPosiblesMovimientos(self):
-        actualidad = copy.deepcopy(self.tablero)
-        # posicionFichaCol = 0
-        # posicionFichaFila = 0
-        arbolListasDeListas = []
-        # listaFichasEncontradas = []
+    def cantidad_proximo_movimiento(self, nueva_columna):
+        cantidad_fichas = 0
+        for fila in range(0, 6):
+            # Contamos las fichas de la columna
+            if self.tablero[fila][nueva_columna] != 0:
+                cantidad_fichas += 1
 
-        # Recorrer la matriz por filas y columnas
+        # Si la ficha cae en una columna vacía, se cambia el jugador y se restablece la cantidad de turnos
+        if cantidad_fichas == 1 or self.cantidadMovimiento == 0:
+            self.cantidadMovimiento = 1
+            self.turnos_restantes = 2
+            self.jugador = 1 if self.jugador == 2 else 2
+        else:
+            # Si la columna tiene más fichas y hay otro turno, se fija la cantidad del
+            # proximo movimiento
+            if self.turnos_restantes == 2:
+                self.turnos_restantes = 1
+                self.cantidadMovimiento = cantidad_fichas - 1
+
+            # Si solo le quedaba un turno, se pasa el turno al siguiente jugador
+            else:
+                self.cantidadMovimiento = 1
+                self.turnos_restantes = 2
+                self.jugador = 1 if self.jugador == 2 else 2
+
+    def calcular_movimiento(self, fila_ficha, columna_ficha):
+        # Entra cuando la ficha es 1 o 2
+        if self.tablero[fila_ficha][columna_ficha] == self.jugador:
+            # Evaluar si el el jugador real
+            if self.jugador == 1:
+                nueva_columna = columna_ficha + self.cantidadMovimiento
+                if nueva_columna > 7:
+                    nueva_columna = 7
+                nueva_posicion = self.buscar_espacio_libre(nueva_columna)
+                # Si no encontró una posición libre
+                if not nueva_posicion:
+                    # Si la ficha roja ya sale del juego
+                    if nueva_columna >= 7:
+                        # Se actualiza una copia del tablero con el movimiento
+                        nuevo_tablero = copy.deepcopy(self.tablero)
+                        nuevo_tablero[fila_ficha][columna_ficha] = 0
+
+                        # Se crea una nueva instancia con el tablero actualizado
+                        linja_siguiente_movimiento = Linja(
+                            nuevo_tablero,
+                            self.jugador,
+                            self.turnos_restantes,
+                            self.cantidadMovimiento,
+                            self.fichasMetaRoja,
+                            self.fichasMetaNegra,
+                        )
+
+                        linja_siguiente_movimiento.cantidadMovimiento = 0
+
+                        # Se calcula si hay próximo movimiento y quién sigue
+                        linja_siguiente_movimiento.cantidad_proximo_movimiento(
+                            nueva_columna
+                        )
+                        linja_siguiente_movimiento.fichasMetaRoja += 1
+
+                        # Se retorna la instancia actualizada
+                        return linja_siguiente_movimiento
+
+                # Si se encuentran espacios disponibles
+                else:
+                    # Se actualiza una copia del tablero con el movimiento
+                    nuevo_tablero = copy.deepcopy(self.tablero)
+                    nuevo_tablero[fila_ficha][columna_ficha] = 0
+                    nuevo_tablero[nueva_posicion[0]][nueva_posicion[1]] = 1
+
+                    # Se crea una nueva instancia con el tablero actualizado
+                    linja_siguiente_movimiento = Linja(
+                        nuevo_tablero,
+                        self.jugador,
+                        self.turnos_restantes,
+                        self.cantidadMovimiento,
+                        self.fichasMetaRoja,
+                        self.fichasMetaNegra,
+                    )
+
+                    # Se calcula si hay próximo movimiento y quién sigue
+                    linja_siguiente_movimiento.cantidad_proximo_movimiento(
+                        nueva_columna
+                    )
+
+                    # Se retorna la instancia actualizada
+                    return linja_siguiente_movimiento
+
+            # Evaluar si es la IA
+            else:
+                nueva_columna = columna_ficha - self.cantidadMovimiento
+                if nueva_columna < 0:
+                    nueva_columna = 0
+                nueva_posicion = self.buscar_espacio_libre(nueva_columna)
+                # Si no encontró una posición libre
+                if not nueva_posicion:
+                    # Si la ficha roja ya sale del juego
+                    if nueva_columna <= 0:
+                        # Se actualiza una copia del tablero con el movimiento
+                        nuevo_tablero = copy.deepcopy(self.tablero)
+                        nuevo_tablero[fila_ficha][columna_ficha] = 0
+
+                        # Se crea una nueva instancia con el tablero actualizado
+                        linja_siguiente_movimiento = Linja(
+                            nuevo_tablero,
+                            self.jugador,
+                            self.turnos_restantes,
+                            self.cantidadMovimiento,
+                            self.fichasMetaRoja,
+                            self.fichasMetaNegra,
+                        )
+
+                        linja_siguiente_movimiento.cantidadMovimiento = 0
+
+                        # Se calcula si hay próximo movimiento y quién sigue
+                        linja_siguiente_movimiento.cantidad_proximo_movimiento(
+                            nueva_columna
+                        )
+                        linja_siguiente_movimiento.fichasMetaNegra += 1
+
+                        # Se retorna la instancia actualizada
+                        return linja_siguiente_movimiento
+
+                # Si se encuentran espacios disponibles
+                else:
+                    # Se actualiza una copia del tablero con el movimiento
+                    nuevo_tablero = copy.deepcopy(self.tablero)
+                    nuevo_tablero[fila_ficha][columna_ficha] = 0
+                    nuevo_tablero[nueva_posicion[0]][nueva_posicion[1]] = 2
+
+                    # Se crea una nueva instancia con el tablero actualizado
+                    linja_siguiente_movimiento = Linja(
+                        nuevo_tablero,
+                        self.jugador,
+                        self.turnos_restantes,
+                        self.cantidadMovimiento,
+                        self.fichasMetaRoja,
+                        self.fichasMetaNegra,
+                    )
+
+                    # Se calcula si hay próximo movimiento y quién sigue
+                    linja_siguiente_movimiento.cantidad_proximo_movimiento(
+                        nueva_columna
+                    )
+
+                    # Se retorna la instancia actualizada
+                    return linja_siguiente_movimiento
+        return None
+
+    def posibles_movimientos(self):
+        primer_movimiento = []
+        segundo_movimiento = []
+
+        if not self.fin_del_juego():
+            # Recorrer la matriz por filas y columnas (primer movimiento)
+            for fila in range(6):
+                for columna in range(8):
+                    # Calcular el movimiento de la ficha actual
+                    nuevo_tablero = self.calcular_movimiento(fila, columna)
+                    if nuevo_tablero:
+                        primer_movimiento.append(nuevo_tablero)
+            print("Primer movimiento".center(50, "-"))
+            self.impresion(primer_movimiento)
+            print("\n")
+
+            print("Segundo movimiento".center(50, "-"))
+
+            # Recorrer las posibilidades del primer movimiento para el segundo movimiento
+            i = 0  # Para propositos de print
+            for tablero in primer_movimiento:
+                # Recorrer la matriz por filas y columnas (primer movimiento)
+                for fila in range(6):
+                    for columna in range(8):
+                        # Calcular el movimiento de la ficha actual
+                        nuevo_tablero = tablero.calcular_movimiento(fila, columna)
+                        if nuevo_tablero:
+                            segundo_movimiento.append(nuevo_tablero)
+                            print(f"Posibilidades para el movimiento {i}")
+                            self.impresionSegundo(nuevo_tablero)
+                print("\n")
+                i += 1
+
+        else:
+            print("\n" + "Fin del juego".center(50, "-"), end="\n")
+
+        return copy.deepcopy(segundo_movimiento)
+
+    def impresion(self, lista_movimientos: list):
+        for movimiento in lista_movimientos:
+            for fila in range(6):
+                print(movimiento.tablero[fila])
+            print(
+                f"Fichas meta negra: {movimiento.fichasMetaNegra}".center(50, "-")
+                + f"Fichas meta roja: {movimiento.fichasMetaRoja}".center(50, "-"),
+                end="\n",
+            )
+
+    def impresionSegundo(self, movimiento: list):
         for fila in range(6):
-            for columna in range(8):
-                # Entra cuando la ficha es 1 o 2
-                if actualidad[fila][columna] == self.jugador:
-                    # Evaluar si el el jugador real
-                    if self.jugador == 1:
-                        nueva_columna = columna + self.cantidadMovimiento
-                        if nueva_columna > 7:
-                            nueva_columna = 7
-                        nueva_posicion = self.buscar_espacio_libre(nueva_columna)
-                        # Si no encontró una posición libre
-                        if not nueva_posicion:
-                            # Si la ficha roja ya sale del juego
-                            if nueva_columna >= 7:
-                                nuevo_tablero = copy.deepcopy(actualidad)
-                                nuevo_tablero[fila][columna] = 0
-                                arbolListasDeListas.append(nuevo_tablero)
-                                self.fichasMetaRoja += 1
-                        else:
-                            nuevo_tablero = copy.deepcopy(actualidad)
-                            nuevo_tablero[fila][columna] = 0
-                            nuevo_tablero[nueva_posicion[0]][nueva_posicion[1]] = 1
-                            arbolListasDeListas.append(nuevo_tablero)
+            print(movimiento.tablero[fila])
+        print(
+            f"Fichas meta negra: {movimiento.fichasMetaNegra}".center(50, "-")
+            + f"Fichas meta roja: {movimiento.fichasMetaRoja}".center(50, "-"),
+            end="\n",
+        )
 
-                    # Evaluar si es la IA
-                    else:
-                        nueva_columna = columna - self.cantidadMovimiento
-                        nueva_posicion = self.buscar_espacio_libre(nueva_columna)
-                        # Si no encontró una posición libre
-                        if not nueva_posicion:
-                            # Si la ficha negra ya sale del juego
-                            if nueva_columna <= 0:
-                                nuevo_tablero = copy.deepcopy(actualidad)
-                                nuevo_tablero[fila][columna] = 0
-                                arbolListasDeListas.append(nuevo_tablero)
-                                self.fichasMetaNegra += 1
-                        else:
-                            nuevo_tablero = copy.deepcopy(actualidad)
-                            nuevo_tablero[fila][columna] = 0
-                            nuevo_tablero[nueva_posicion[0]][nueva_posicion[1]] = 2
-                            arbolListasDeListas.append(nuevo_tablero)
-        self.impresion(arbolListasDeListas)
 
-        # for fila in range(
-        #     5, -1, -1
-        # ):  # for para movernos en el tablero, filas, empieza desde 0 por indexaccion
-        #     for columna in range(
-        #         7, -1, -1
-        #     ):  # for para movernos en el tablero, columnas
-        #         if [
-        #             fila,
-        #             columna,
-        #         ] not in listaFichasEncontradas:  # si ya se ha encontrado una ficha, ignorar esa posicion en especifico
-        #             if (
-        #                 2 == self.tablero[fila][columna]
-        #             ):  # buscamos la ficha que mueve la ia, en este caso es el 2
-        #                 posicionFichaFila = fila
-        #                 posicionFichaCol = columna
-
-        #                 listaFichasEncontradas.append(
-        #                     [posicionFichaFila, posicionFichaCol]
-        #                 )
-
-        #                 if (
-        #                     columna != 0
-        #                 ):  # si esta en la ultima columna ignoramos pq ya llego a ultima casilla
-        #                     if (
-        #                         actualidad[fila][columna - self.cantidadMovimiento] == 0
-        #                     ):  # preguntamos si puede avanzar, si el espacio esta disponible
-        #                         nuevoTablero = copy.deepcopy(actualidad)
-        #                         nuevoTablero[fila][
-        #                             columna - self.cantidadMovimiento
-        #                         ] = 2  # avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
-        #                         nuevoTablero[posicionFichaFila][
-        #                             posicionFichaCol
-        #                         ] = 0  # como avanzamos tenemos que eliminar la ficha de donde estaba
-        #                         arbolListasDeListas.append(nuevoTablero)
-        #                         bandera = False
-        #                         espacioLibre = True
-
-        #                     else:
-        #                         respuesta = self.buscar_espacio_libre(
-        #                             columna - self.cantidadMovimiento
-        #                         )
-
-        #                         if (
-        #                             respuesta
-        #                         ):  # sise ha encontrado un espacio libre en la misma columna... si no se encuentra pss no puede saltar, pq lo minimo para saltar seria 2 si la siguiente columna de la anterior esta disponible
-        #                             nuevoTablero = copy.deepcopy(actualidad)
-        #                             nuevoTablero[respuesta[0]][
-        #                                 respuesta[1]
-        #                             ] = 2  # avanzmaos en el tablero, la ficha elegida en una posicion que si pueda estar
-        #                             nuevoTablero[posicionFichaFila][
-        #                                 posicionFichaCol
-        #                             ] = 0  # como avanzamos tenemos que eliminar la ficha de donde estaba
-        #                             arbolListasDeListas.append(nuevoTablero)
-        #                             espacioLibre = True
-        #                             # break
-
-        #                         elif (
-        #                             columna - self.cantidadMovimiento == 0
-        #                         ):  # Si es la ulima columna ni no esta disponible
-        #                             nuevoTablero = copy.deepcopy(actualidad)
-        #                             nuevoTablero[posicionFichaFila][
-        #                                 posicionFichaCol
-        #                             ] = 0  # como avanzamos tenemos que eliminar la ficha de donde estaba
-        #                             self.fichasMetaNegra += 1  # aumentamos las fichas que han llegando a la meta en 1 para tener un conteo
-        #                             arbolListasDeListas.append(nuevoTablero)
-
-        #                 if (
-        #                     columna - self.cantidadMovimiento < 0
-        #                 ):  # la ficha ya llego al final del tablero y se pierden los movimientos que se tengan hasta el momento
-        #                     print("Out of bounds")
-        #                     nuevoTablero = copy.deepcopy(actualidad)
-        #                     nuevoTablero[posicionFichaFila][
-        #                         posicionFichaCol
-        #                     ] = 0  # como avanzamos tenemos que eliminar la ficha de donde estaba
-        #                     arbolListasDeListas.append(nuevoTablero)
-        #                     bandera = False
-        #                     espacioLibre = True
-        #                     self.fichasMetaNegra += 1
-
-        # self.impresion(arbolListasDeListas)
-        # print(listaFichasEncontradas)
-
+# matriz = [
+#     [2, 2, 0, 0, 0, 0, 1, 1],
+#     [2, 2, 0, 0, 0, 0, 1, 1],
+#     [2, 2, 0, 0, 0, 0, 1, 1],
+#     [2, 0, 0, 0, 1, 2, 0, 1],
+#     [2, 2, 0, 0, 0, 0, 1, 1],
+#     [2, 2, 0, 0, 0, 0, 1, 1],
+# ]
 
 matriz = [
     [1, 2, 2, 2, 2, 2, 2, 2],
@@ -170,5 +271,7 @@ matriz = [
     [1, 0, 0, 0, 0, 0, 0, 2],
     [1, 1, 1, 1, 1, 1, 1, 2],
 ]
-linja = Linja(tablero=matriz, jugador=1, cantidadMovimiento=2)
-linja.avanzarPosiblesMovimientos()
+linja = Linja(
+    matriz,
+)
+# linja.posibles_movimientos()
